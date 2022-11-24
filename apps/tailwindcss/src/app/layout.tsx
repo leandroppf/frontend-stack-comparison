9e201195d78de46094c925341500b374d296129c
Leandro Pedroso <lpedroso@meudroz.com>
2022-11-23 23:56:52 -0300

    Added title to tailwindcss pages

--- apps/tailwindcss/src/app/layout.tsx
+++ apps/tailwindcss/src/app/layout.tsx
@@ -8,7 +8,9 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <head></head>
+      <head>
+        <title>Tailwindcss stack</title>
+      </head>
       <body>
         <ClientLayout>{children}</ClientLayout>
       </body>
