c8170935aec7e7ef5906a06bc9f3091486269d00
Yago Russo <yagrrusso@gmail.com>
2022-11-25 12:23:07 -0300

    js and tailwind differences

--- apps/stitches/src/app/page.tsx
+++ apps/stitches/src/app/page.tsx
@@ -3,7 +3,46 @@ import * as Components from "./styles";
 export default function Docs() {
   return (
     <Components.Main>
-      <Components.PageTitle>Stitches</Components.PageTitle>
+      <Components.FlexColumn center>
+        <Components.FlexRow center>
+          <Components.Text color="red" size="sm">
+            Stitches
+          </Components.Text>
+          <Components.Text color="green">Next</Components.Text>
+          <Components.Text color="blue" size="lg">
+            React
+          </Components.Text>
+        </Components.FlexRow>
+        <Components.FlexRow center>
+          <Components.Text color="red" size="sm">
+            Stitches
+          </Components.Text>
+          <Components.Text color="green">Next</Components.Text>
+          <Components.Text color="blue" size="lg">
+            React
+          </Components.Text>
+        </Components.FlexRow>
+      </Components.FlexColumn>
+      <Components.FlexColumn center border>
+        <Components.FlexRow center>
+          <Components.Text color="red" size="sm">
+            Stitches
+          </Components.Text>
+          <Components.Text color="green">Next</Components.Text>
+          <Components.Text color="blue" size="lg">
+            React
+          </Components.Text>
+        </Components.FlexRow>
+        <Components.FlexRow center>
+          <Components.Text color="red" size="sm">
+            Stitches
+          </Components.Text>
+          <Components.Text color="green">Next</Components.Text>
+          <Components.Text color="blue" size="lg">
+            React
+          </Components.Text>
+        </Components.FlexRow>
+      </Components.FlexColumn>
     </Components.Main>
   );
 }
